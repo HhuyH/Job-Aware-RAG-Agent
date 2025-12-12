eda816f2e4834b4c3540505c5c162bf437ef4fcd
HhuyH <hoanhuy12@gmail.com>
2025-12-12 14:52:24 +0700

    Rename folder database → knowledge_base

--- temp.py
+++
@@ -1,9 +0,0 @@
-# ------- frozenset là phiên bản bất biến của một tập hợp.-------
-
-# Giống như sets, frozenset chứa các phần tử duy nhất, không có thứ tự và không thể thay đổi.
-# Không giống như sets, các phần tử không thể được thêm hoặc bớt khỏi frozenset.
-
-# dùng frozenset() để tạo frozenset
-x = frozenset({"apple", "banana", "cherry"})
-print(x) # Kết quả: frozenset({'cherry', 'banana', 'apple'})
-print(type(x)) # Kết quả: <class 'frozenset'>
